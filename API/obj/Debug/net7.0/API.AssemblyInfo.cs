a80e123ed0d7c90840cf3104be5d7fb74303147c
Pablo Pasquim <pablo.pasquim7@gmail.com>
2024-05-24 22:05:18 -0300

    Jogo da Velha

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf924b5a7948e163cc367f03e836ddf2a3703ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
